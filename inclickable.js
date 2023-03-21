cb3a6ef5f253c0bab569016f0d23bb8675568773
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2023-03-21 09:12:31 +0100

    inclickable

---
+++ inclickable.js
@@ -0,0 +1,48 @@
+//aggancio la funzione di gestione al completamento del DOM
+document.addEventListener("DOMContentLoaded", manager);
+
+function manager(){
+    let caatch = document.getElementById("catch");
+
+      //aggancio le funzioni agli eventi dei DIV di controllo
+      document.getElementById("contsq1").addEventListener("mouseover", from1);
+      document.getElementById("contsq2").addEventListener("mouseover", from2);
+      document.getElementById("contsq3").addEventListener("mouseover", from3);
+      document.getElementById("contsq4").addEventListener("mouseover", from4);
+
+
+    function from1(){
+        add_class("sq2");
+
+    }
+
+    function from2(){
+        add_class("sq3");
+    }
+
+    function from3(){
+        add_class("sq4");
+    }
+
+    function from4(){
+        add_class("sq1");
+    }
+
+
+
+    function add_class(my_class){
+        
+
+        remove_classes();
+        caatch.classList.add(my_class);
+
+    }
+
+    function remove_classes(){
+        caatch.classList.remove("sq1");
+        caatch.classList.remove("sq2");
+        caatch.classList.remove("sq3");
+        caatch.classList.remove("sq4");
+    }
+
+}
